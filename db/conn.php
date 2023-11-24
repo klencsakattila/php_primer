ccfd78d9062f0d26463bce8bf86899ace7b85005
klencsakattila <klencsakattila@users.noreply.github.com>
2023-11-24 08:53:23 +0100

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php 
-    // Development Connection
-    // $host = '127.0.0.1';
-    // $db = 'attendance_db';
-    // $user = 'root';
-    // $pass = '';
-    // $charset = 'utf8mb4';
+    //Development Connection
+    $host = '127.0.0.1';
+    $db = 'attendance_db';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
 
     //Remote Database Connection
-    $host = 'remotemysql.com';
+    /*$host = 'remotemysql.com';
     $db = 'NC1HPsPMvA';
     $user = 'NC1HPsPMvA';
     $pass = 'rqUDdNcsXH';
-    $charset = 'utf8mb4';
+    $charset = 'utf8mb4';*/
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
@@ -19,7 +19,8 @@
         $pdo = new PDO($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    } catch(PDOException $e) {
+    } 
+    catch(PDOException $e) {
         throw new PDOException($e->getMessage());
     }
 
